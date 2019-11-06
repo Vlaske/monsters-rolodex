e7edcfe8fdccba4fad7bc74c7b8cd32a66e1dff6
Mike Manojlovic <mike.manojlovic@gmail.com>
2019-11-06 01:18:53 +0100

    Updates

---
+++ precache-manifest.ea694906710f356ec9d92f2872fe0ed8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218ac9223ada845378017dd45fc68a98",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cbe3f99deff0362973b9",
+    "url": "/monsters-rolodex/static/css/main.987abab1.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "cbe3f99deff0362973b9",
+    "url": "/monsters-rolodex/static/js/main.a3db0b51.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
